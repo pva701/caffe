a9b19c056eb7cdc4605356211fe8828602a4332a
Ilya Peresadin <pva701@gmail.com>
2016-05-01 20:12:05 +0300

    fixed forward script

--- examples/openface/forward_train_test.sh
+++ examples/openface/forward_train_test.sh
@@ -1,4 +1,4 @@
 set -e
-./forward_all.py --model openface_test.prototxt --weights data/snapshots_iter_$1.caffemodel --leveldb data/val_trainset96 --output trainset96
+./forward_all.py --model openface_test.prototxt --weights data/snapshots_iter_$1.caffemodel --leveldb data/val_trainset --output trainset --limit 1000
 
-./forward_all.py --model openface_test.prototxt --weights data/snapshots_iter_$1.caffemodel --leveldb data/val_testset96 --output mnist_testset96
+./forward_all.py --model openface_test.prototxt --weights data/snapshots_iter_$1.caffemodel --leveldb data/val_testset --output testset
